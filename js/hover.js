604ebf20716f526867aa5a3eadcde1b7ae8946ae
Greigh <57759540+Greigh@users.noreply.github.com>
2021-01-08 16:00:29 -0500

    logo update

---
+++ js/hover.js
@@ -0,0 +1,54 @@
+//GitHub Hover
+function GitHubhover(element) {
+    element.setAttribute('src', './images/Github-hover.png');
+}
+
+//GitHub unhover
+function GitHubunhover(element) {
+    element.setAttribute('src', './images/Github.png');
+}
+
+//Twitter hover
+function Twitterhover(element) {
+    element.setAttribute('src', './images/Twitter-hover.png');
+}
+
+//Twitter unhover
+function Twitterunhover(element) {
+    element.setAttribute('src', './images/Twitter.png');
+}
+//Apple Terminal hover
+function AppleTermhover(element) {
+    element.setAttribute('src', './images/ATLogo-hover.png');
+}
+
+//Apple Terminal unhover
+function AppleTermunhover(element) {
+    element.setAttribute('src', './images/ATLogo.png');
+}
+
+//Lgenia hover
+function Lgeniahover(element) {
+    element.setAttribute('src', './images/Lgenia-hover.png');
+}
+
+//Lgenia unhover
+function Lgeniaunhover(element) {
+    element.setAttribute('src', './images/Lgenia.png');
+}
+
+function Mediumhover(element) {
+    element.setAttribute('src', './images/Medium-hover.png');
+}
+
+function Mediumunhover(element) {
+    element.setAttribute('src', './images/Medium.png');
+}
+
+function Telegramhover(element) {
+    element.setAttribute('src', './images/Telegram-hover.png');
+}
+
+function Telegramunhover(element) {
+    element.setAttribute('src', './images/Telegram.png');
+}
